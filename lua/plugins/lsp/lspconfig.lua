444247cafd7ba0404cb88ec297ee4a3437a11e0e
EugenCazacu <29628413+EugenCazacu@users.noreply.github.com>
2025-06-25 08:22:22 +0300

    Update lspconfig.lua

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -7,9 +7,6 @@ return {
         { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-        local lspconfig = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local keymap = vim.keymap
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -60,32 +57,22 @@ return {
                 keymap.set("n", "<leader>td", function()
                     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
                 end, opts)
-
             end,
         })
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["lua_ls"] = function()
-                lspconfig["lua_ls"].setup({
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            -- make the language server recognize "vim" global
-                            diagnostics = {
-                                globals = { "vim" },
-                            },
-                            -- completion = {
-                            --     callSnippet = "Replace",
-                            -- },
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                    },
+                    diagnostics = {
+                        globals = {
+                            "vim",
+                            "require",
                         },
                     },
-                })
-            end,
+                },
+            },
         })
-    end
+    end,
 }
