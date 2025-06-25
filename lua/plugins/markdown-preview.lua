5f219496fbced171fdc3b6aba55374b6c37957c3
EugenCazacu <29628413+EugenCazacu@users.noreply.github.com>
2025-06-25 08:18:49 +0300

    Update markdown-preview.lua

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,10 +1,15 @@
+-- TODO: Note that this plugin is no longer maintained
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
+    -- build = function()
+    --     vim.fn["mkdp#util#install"]()
+    -- end,
+    build = ":call mkdp#util#install()",
+    -- or if not working:
+    -- cd ~/.local/share/nvim/lazy/markdown-preview.nvim
+    -- npm install
     config = function()
         local keymap = vim.keymap
         keymap.set("n", "<leader>md", vim.cmd.MarkdownPreviewToggle, { desc = "Start/toggle markdown preview" })
